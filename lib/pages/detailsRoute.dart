0837ec43e5380f3d034427001aab3bfb85dc6e5d
vincenttarrit <vincent@tarrit.com>
2020-03-14 22:07:32 +0100

    details route

--- lib/pages/detailsRoute.dart
+++ lib/pages/detailsRoute.dart
@@ -20,29 +20,53 @@ class DetailsRoute extends StatelessWidget {
           customRow("Départ", c.from),
           customRow("Arrivée", c.to),
           customRow("Durée", Format.intToSeconds(c.duration)),
-          Divider(),
-          customWay(c.legs[0]),
+          const Divider(),
+          customWay(c.legs),
         ],
       ),
     );
   }
 
-  Widget customRow(String key, dynamic value){
+  Widget customRow(String key, dynamic value) {
     return Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[Text(key), Text(value.toString())],
-            ),
-          );
+      padding: const EdgeInsets.all(8.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[Text(key), Text(value.toString())],
+      ),
+    );
   }
-  Widget customWay(Legs legs){
-    return Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[Text(legs.type), Text(legs.arrival.toString())],
-            ),
-          );
+
+  Widget customWay(List<Legs> legs) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: buildLegs(legs),
+    );
+  }
+
+  List<Widget> buildLegs(List<Legs> legs) {
+    List<Widget> list = [];
+    for (var i = 0; i < legs.length; i++) {
+      Legs l = legs[i];
+      Legs nextLegs = i == legs.length-1 ? legs[i] : legs[i+1];
+      print("type: ${l.type}");
+      if (l.type != null) {
+        list.add(Container(
+            height: 100,
+            color: Colors.red.withOpacity(0.2),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(l.type.toString()),
+                Text(l.name),
+                Text(nextLegs.name),
+                if (l.arrival != null)
+                  Format.duration(l.arrival.difference(DateTime.now()))
+              ],
+            )));
+        list.add(Divider());
+      }
+    }
+    return list;
   }
 }
