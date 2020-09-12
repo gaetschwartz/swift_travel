aa4d0fb974a5ff10e8f5ce7fa106607d7eec7b40
Blax <blaxoujunior@gmail.com>
2020-09-13 00:29:31 +0200

    wut

--- CI/publish_testflight.sh
+++ CI/publish_testflight.sh
@@ -2,6 +2,11 @@
 
 set -eo pipefail
 
+cp $PWD/build/Runner.ipa $PWD/build/Runner-ipa.zip
+unzip $PWD/build/Runner-ipa.zip
+ls $PWD/build/Runner-ipa
+echo $PWD/build/Runner-ipa/Info.list
+
 xcrun altool --upload-app\
   -t ios \
   -f $PWD/build/Runner.ipa \
