ce348fb8c242ece15b690fc0fc5f674b19199a13
Blax <blaxoujunior@gmail.com>
2020-09-23 07:25:13 +0200

    stoodi

--- CI/prebuild.sh
+++ CI/prebuild.sh
@@ -3,14 +3,14 @@
 set -e
 # debug log
 set -x
-git_hash=$(git rev-parse --short "$GITHUB_SHA")
-git_msg=$(printf "%q" "$(git log -1 --pretty=format:"%s")")
+git_hash=$GIT_CLONE_COMMIT_HASH
+git_msg=$(printf "%q" "$GIT_CLONE_COMMIT_MESSAGE_BODY")
 now=$(date +"%H:%M:%S - %A %d %B %Y")
 cat > lib/utils/build.dart <<- EOM
 const String commitBuildDate = "${now}";
 const String commitHash = "${git_hash}";
 const String commitMessage = "${git_msg}";
-const String buildNumber = "$GITHUB_RUN_NUMBER";
+const String buildNumber = "$GIT_CLONE_COMMIT_COUNT";
 EOM
 echo "=== build.dart ==="
 cat lib/utils/build.dart
