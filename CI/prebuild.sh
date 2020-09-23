dc0ad8390711e8b294234ad1937463e25f59ea3c
Blax <blaxoujunior@gmail.com>
2020-09-23 07:15:14 +0200

    fix rebuild.sh

--- CI/prebuild.sh
+++ CI/prebuild.sh
@@ -1,3 +1,8 @@
+#!/usr/bin/env bash
+# fail if any commands fails
+set -e
+# debug log
+set -x
 git_hash=$(git rev-parse --short "$GITHUB_SHA")
 git_msg=$(printf "%q" "$(git log -1 --pretty=format:"%s")")
 now=$(date +"%H:%M:%S - %A %d %B %Y")
@@ -10,4 +15,4 @@ EOM
 echo "=== build.dart ==="
 cat lib/utils/build.dart
 echo "=================="
-pod deingrate
+pod deintegrate
