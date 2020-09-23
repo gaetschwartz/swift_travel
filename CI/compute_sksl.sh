3adf7d8afe683c9d54bd677d94c7724c395c1ad3
Gaëtan Maxime Schwartz <gaetan.schwartz@epfl.ch>
2020-09-23 23:47:04 +0200

    sksl ??

--- CI/compute_sksl.sh
+++ CI/compute_sksl.sh
@@ -1,6 +1,17 @@
+#!/usr/bin/env bash
+# fail if any commands fails
+set -e
+# debug log
+set -x
+
 #flutter drive --target=test_driver/app.dart --profile --cache-sksl --write-sksl-on-exit shaders/01.sksl.json
-FILE=$BITRISE_SOURCE_DIR/shaders/01.sksl.json
+FILE=$(find $BITRISE_SOURCE_DIR/shaders/ -maxdepth 1 -name "*.sksl.json" -print | sort -rn | head -n 1)
 if test -f "$FILE"; then
   export SKSL_ARGUMENTS="--bundle-sksl-path $FILE"
+  echo "Found $FILE."
+else 
+  echo "Didn't find any sksl file."
 fi
+# find . -maxdepth 1 -name "*string*" ! -name "*:*" -print
+
 
