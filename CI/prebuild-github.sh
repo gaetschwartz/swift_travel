a5aa21cb74de7983108842f18c067c00a8925061
Gaëtan Maxime Schwartz <gaetan.schwartz@epfl.ch>
2020-09-25 00:37:46 +0200

    please build

--- CI/prebuild-github.sh
+++ CI/prebuild-github.sh
@@ -6,12 +6,12 @@ set -x
 git_hash=$(git rev-parse --short "$GITHUB_SHA")
 git_msg=$(printf "%q" "$(git log -1 --pretty=format:"%s")")
 now=$(date +"%H:%M:%S - %A %d %B %Y")
-nbr=$(git rev-list --count origin/$(git rev-parse --abbrev-ref HEAD))
+
 cat > lib/utils/build.dart <<- EOM
 const String commitBuildDate = "${now}";
 const String commitHash = "${git_hash}";
 const String commitMessage = "${git_msg}";
-const String buildNumber = "${nbr}";
+const String buildNumber = "${COMMIT_COUNT}";
 EOM
 echo "=== build.dart ==="
 cat lib/utils/build.dart
