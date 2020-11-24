db60cd25a1c3de7366157c4a98e8597e58e8888d
Gaëtan <blaxoujunior@gmail.com>
2020-11-24 17:13:28 +0100

    fix

--- CI/prebuild.sh
+++ CI/prebuild.sh
@@ -2,9 +2,9 @@
 set -e
 set -x
 
-hash=$([[ $BITRISE_IO ]] && $GIT_CLONE_COMMIT_HASH || $GIT_HASH)
-msg=$([[ $BITRISE_IO ]] && $BITRISE_GIT_MESSAGE || $GIT_MSG)
-count=$([[ $BITRISE_IO ]] && $GIT_CLONE_COMMIT_COUNT || $COMMIT_COUNT)
+hash=$([[ $BITRISE_IO ]] && echo $GIT_CLONE_COMMIT_HASH || echo $GIT_HASH)
+msg=$([[ $BITRISE_IO ]] && echo $BITRISE_GIT_MESSAGE || echo $GIT_MSG)
+count=$([[ $BITRISE_IO ]] && echo $GIT_CLONE_COMMIT_COUNT || echo $COMMIT_COUNT)
 now=$(date +"%H:%M:%S - %A %d %B %Y")
 
 cat > lib/constants/build.dart <<- EOM
