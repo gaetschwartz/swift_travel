20c28689714211154ac3ae82a5abf0d2aea31692
Gaëtan <blaxoujunior@gmail.com>
2021-02-23 15:53:13 +0100

    fix pathss

--- CI/sign_apk.sh
+++ CI/sign_apk.sh
@@ -1,3 +1,3 @@
 cd $ANDROID_HOME/build-tools/30.0.2/
-./apksigner sign -ks android/app/key.jks --ks-pass pass:$PASSWORD build/app/outputs/flutter-apk/app-release.apk
+./apksigner sign -ks ./android/app/key.jks --ks-pass pass:$PASSWORD ./build/app/outputs/flutter-apk/app-release.apk
 
