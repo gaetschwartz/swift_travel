495f0da850ab8ce462c05f4646d593b9a7bb114d
Gaëtan <blaxoujunior@gmail.com>
2021-02-23 16:03:11 +0100

    finally fixed ?

--- CI/sign_apk.sh
+++ CI/sign_apk.sh
@@ -1,3 +1,2 @@
-cd $ANDROID_HOME/build-tools/30.0.2/
-./apksigner sign -ks ./android/app/key.jks --ks-pass pass:$PASSWORD ./build/app/outputs/flutter-apk/app-release.apk
+$ANDROID_HOME/build-tools/30.0.2/apksigner sign -ks ./android/app/key.jks --ks-pass pass:$PASSWORD ./build/app/outputs/flutter-apk/app-release.apk
 
