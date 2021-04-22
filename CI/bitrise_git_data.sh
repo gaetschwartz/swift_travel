0db01d95184e7fde6a3445cd82c2d67e93085d58
Gaëtan <blaxoujunior@gmail.com>
2021-04-22 17:02:23 +0200

    fix bitrise commit count

--- CI/bitrise_git_data.sh
+++ CI/bitrise_git_data.sh
@@ -4,6 +4,6 @@ set -x
 
 envman add --key GIT_SHORT_SHA --value "$GIT_CLONE_COMMIT_HASH"
 envman add --key COMMIT_MSG --value "$BITRISE_GIT_MESSAGE"
-envman add --key GIT_CLONE_COMMIT_COUNT --value "$API_BITRISE_SCHEME"
+envman add --key COMMIT_NUMBER --value "$GIT_CLONE_COMMIT_COUNT"
 envman add --key APP_VERSION --value "$(bash $BITRISE_SOURCE_DIR/CI/app_version.sh $BITRISE_SOURCE_DIR)"
 envman add --key GOT_GIT_DATA --value "true"
