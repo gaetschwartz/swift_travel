03658dfc1aace1a179cd0d0daa34eac9babf662b
Gaëtan <blaxoujunior@gmail.com>
2021-04-22 16:51:44 +0200

    fix app version

--- CI/github_git_data.sh
+++ CI/github_git_data.sh
@@ -5,5 +5,5 @@ set -x
 echo "GIT_SHORT_SHA=$(git rev-parse --short "$GITHUB_SHA")" >> $GITHUB_ENV
 echo "COMMIT_MSG=$(git log -1 --pretty=format:"%s")" >> $GITHUB_ENV
 echo "COMMIT_NUMBER=$(git rev-list --count HEAD)" >> $GITHUB_ENV
-echo "APP_VERSION=$(bash $GITHUB_WORKSPACE/CI/app_version.sh)" >> $GITHUB_ENV
+echo "APP_VERSION=$(bash $GITHUB_WORKSPACE/CI/app_version.sh $GITHUB_WORKSPACE)" >> $GITHUB_ENV
 echo "GOT_GIT_DATA=true" >> $GITHUB_ENV
