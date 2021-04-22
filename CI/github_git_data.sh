30f82126c1d7485b14055b01bfa401270b8a2b01
Gaëtan <blaxoujunior@gmail.com>
2021-04-22 16:02:25 +0200

    improve ci

--- CI/github_git_data.sh
+++ CI/github_git_data.sh
@@ -1,4 +1,4 @@
-echo "::set-output name=hash::$(git rev-parse --short "$GITHUB_SHA")"
-echo "::set-output name=msg::$(git log -1 --pretty=format:"%s")"
-echo "::set-output name=count::$(git rev-list --count HEAD)"
-echo "::set-output name=version::$(grep 'version:' $GITHUB_WORKSPACE/app/pubspec.yaml | awk '{print $2}' | cut -d "+" -f1)"
+echo "GITHUB_SHORT_SHA=$(git rev-parse --short "$GITHUB_SHA")" >> $GITHUB_ENV
+echo "COMMIT_MSG=$(git log -1 --pretty=format:"%s")" >> $GITHUB_ENV
+echo "COMMIT_NUMBER=$(git rev-list --count HEAD)" >> $GITHUB_ENV
+echo "APP_VERSION=$(grep 'version:' $GITHUB_WORKSPACE/app/pubspec.yaml | awk '{print $2}' | cut -d "+" -f1)" >> $GITHUB_ENV
