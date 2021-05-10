2a58d476fe2e8b004b9d417e3c644d48f6ab7916
Gaëtan <blaxoujunior@gmail.com>
2021-05-10 08:53:22 +0200

    animations for stations search

--- app/lib/constants/env.dart
+++ app/lib/constants/env.dart
@@ -12,6 +12,7 @@ class Env {
   static const spoofLocation = bool.fromEnvironment('SPOOF_LOCATION');
   static const cacheLinesInDebug = !kDebugMode && bool.fromEnvironment('CACHE_LINES_IN_DEBUG');
   static const enableLiveRoute = bool.fromEnvironment('ENABLE_LIVE_ROUTE');
+  static const enableAnimations = true;
 
   static bool boolean(String s) => bool.fromEnvironment(s);
   static String string(String s) => String.fromEnvironment(s);
