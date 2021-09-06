98ab4d4c0cf6d2ba1a64c377d6a87aed089a9364
Gaëtan <blaxoujunior@gmail.com>
2021-09-06 23:01:12 +0200

    fix:  location speed with new geolocator version

--- app/lib/logic/location/location.dart
+++ app/lib/logic/location/location.dart
@@ -1,5 +1,3 @@
-import 'dart:developer';
-
 import 'package:flutter/services.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:swift_travel/constants/env.dart';
@@ -7,39 +5,81 @@ import 'package:swift_travel/logic/location/models/models.dart';
 
 const _spoofedLocation = GeoLocation(latitude: 46.2225454, longitude: 6.1385658);
 
-Future<GeoLocation> getLocation({
-  LocationAccuracy desiredAccuracy = LocationAccuracy.bestForNavigation,
-  bool forceAndroidLocationManager = false,
-  Duration? timeLimit,
-}) async {
-  if (Env.spoofLocation) {
-    return _spoofedLocation;
-  } else {
-    try {
-      LocationPermission permission;
+class GeoLocationEngine {
+  GeoLocationEngine._();
+  static late final instance = GeoLocationEngine._();
 
-      permission = await Geolocator.checkPermission();
-      if (permission == LocationPermission.denied) {
-        permission = await Geolocator.requestPermission();
-      }
-      if (permission == LocationPermission.whileInUse || permission == LocationPermission.always) {
-        final p = await Geolocator.getCurrentPosition(
-          desiredAccuracy: desiredAccuracy,
-          forceAndroidLocationManager: forceAndroidLocationManager,
-          timeLimit: timeLimit,
-        );
-        return GeoLocation.fromPosition(p);
-      } else {
-        throw PermissionDeniedException(permission.toString());
-      }
-    } on MissingPluginException {
-      if (Env.isDebugMode) {
-        log('Location is not supported on this dervice, returned a spoofed location');
-        await Future<void>.delayed(const Duration(seconds: 1));
-        return _spoofedLocation;
-      } else {
-        rethrow;
+  DateTime? _lastUsed;
+
+  Future<GeoLocation> getLocation({
+    LocationAccuracy desiredAccuracy = LocationAccuracy.bestForNavigation,
+    Duration? timeLimit,
+  }) async {
+    if (Env.spoofLocation) {
+      return _spoofedLocation;
+    } else {
+      try {
+        LocationPermission permission = await Geolocator.checkPermission();
+
+        if (permission == LocationPermission.denied) {
+          permission = await Geolocator.requestPermission();
+        }
+
+        if (permission.isDenied) {
+          final now = DateTime.now();
+
+          Position? p;
+
+          if (_lastUsed != null && _lastUsed!.difference(now).inSeconds < 60) {
+            p = await Geolocator.getLastKnownPosition();
+          }
+
+          p ??= await Geolocator.getCurrentPosition(
+            desiredAccuracy: desiredAccuracy,
+            timeLimit: timeLimit,
+          );
+
+          _lastUsed = now;
+
+          return GeoLocation.fromPosition(p);
+        } else {
+          throw PermissionDeniedException(permission.toString());
+        }
+      } on MissingPluginException {
+        if (Env.isDebugMode) {
+          print('Location is not supported on this dervice, returned a spoofed location');
+          return Future<GeoLocation>.delayed(
+            const Duration(milliseconds: 500),
+            () => _spoofedLocation,
+          );
+        } else {
+          rethrow;
+        }
       }
     }
   }
+}
+
+Future<GeoLocation> getLocation({
+  LocationAccuracy desiredAccuracy = LocationAccuracy.bestForNavigation,
+  Duration? timeLimit,
+}) =>
+    GeoLocationEngine.instance.getLocation(
+      desiredAccuracy: desiredAccuracy,
+      timeLimit: timeLimit,
+    );
+
+extension LocationPermissionX on LocationPermission {
+  bool get isAccepted {
+    switch (this) {
+      case LocationPermission.denied:
+      case LocationPermission.deniedForever:
+        return false;
+      case LocationPermission.whileInUse:
+      case LocationPermission.always:
+        return true;
+    }
+  }
+
+  bool get isDenied => !isAccepted;
 }
