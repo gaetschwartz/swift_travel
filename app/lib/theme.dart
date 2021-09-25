3bb6206570a3dfcb9501d5818f994411d86b848d
Gaëtan <blaxoujunior@gmail.com>
2021-09-25 12:23:10 -0700

    update deps

--- app/lib/theme.dart
+++ app/lib/theme.dart
@@ -14,13 +14,11 @@ final MaterialColor redAccent = Colors.redAccent.toMaterialColor();
 final MaterialColor white = Colors.white.toMaterialColor();
 
 const lightShadow = ShadowTheme(
-  buttonShadow:
-      BoxShadow(blurRadius: 8, color: Color(0x200700b1), offset: Offset(0, 4)),
+  buttonShadow: BoxShadow(blurRadius: 8, color: Color(0x200700b1), offset: Offset(0, 4)),
 );
 
 const darkShadow = ShadowTheme(
-  buttonShadow:
-      BoxShadow(blurRadius: 4, color: Color(0x4C000000), offset: Offset(0, 4)),
+  buttonShadow: BoxShadow(blurRadius: 4, color: Color(0x4C000000), offset: Offset(0, 4)),
 );
 
 TextTheme _platform(TextTheme t) => t;
@@ -48,15 +46,9 @@ final ThemeConfiguration themeConfiguration = ThemeConfiguration(
   persist: true,
   applyToAllThemes: (t) {
     return t.copyWith(
-        cupertinoOverrideTheme:
-            CupertinoThemeData(brightness: t.brightness).rawCopy(),
-        pageTransitionsTheme: const PageTransitionsTheme(builders: {
-          TargetPlatform.android: FadeUpwardsPageTransitionsBuilder(),
-          TargetPlatform.iOS: CupertinoPageTransitionsBuilder(),
-          TargetPlatform.linux: FadeUpwardsPageTransitionsBuilder(),
-          TargetPlatform.macOS: CupertinoPageTransitionsBuilder(),
-          TargetPlatform.windows: FadeUpwardsPageTransitionsBuilder(),
-        }));
+      cupertinoOverrideTheme: CupertinoThemeData(brightness: t.brightness).rawCopy(),
+      pageTransitionsTheme: const PageTransitionsTheme(),
+    );
   },
   lightThemes: [
     ExtendedTheme(
@@ -100,8 +92,7 @@ final ThemeConfiguration themeConfiguration = ThemeConfiguration(
     ExtendedTheme(
       name: 'Abin',
       id: 'abin',
-      colorScheme: ColorScheme.fromSwatch(
-          primarySwatch: purpleAbin, brightness: Brightness.dark),
+      colorScheme: ColorScheme.fromSwatch(primarySwatch: purpleAbin, brightness: Brightness.dark),
       shadow: darkShadow,
     ),
   ],
