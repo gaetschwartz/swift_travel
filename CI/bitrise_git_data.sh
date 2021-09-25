635b815e9b21284dc4ed3b622a32bb1742a6d71e
Gaëtan <blaxoujunior@gmail.com>
2021-09-25 22:27:15 +0200

    try something

--- CI/bitrise_git_data.sh
+++ CI/bitrise_git_data.sh
@@ -4,6 +4,6 @@ set -x
 
 envman add --key GIT_SHORT_SHA --value "$GIT_CLONE_COMMIT_HASH"
 envman add --key COMMIT_MSG --value "$BITRISE_GIT_MESSAGE"
-envman add --key COMMIT_NUMBER --value "$GIT_CLONE_COMMIT_COUNT"
+envman add --key COMMIT_NUMBER --value "$([[ ! -z "$GIT_CLONE_COMMIT_COUNT" ]] && echo "$GIT_CLONE_COMMIT_COUNT" || echo "$(git rev-list --count main)")"
 envman add --key APP_VERSION --value "$(bash $BITRISE_SOURCE_DIR/CI/app_version.sh $BITRISE_SOURCE_DIR)"
 envman add --key GOT_GIT_DATA --value "true"
