41153db80e15d4d1300f43d8d21dfe796a588458
Gaëtan <blaxoujunior@gmail.com>
2022-12-01 12:27:14 +0100

    deps

--- packages/theming/lib/src/models.freezed.dart
+++ packages/theming/lib/src/models.freezed.dart
@@ -1,7 +1,7 @@
 // coverage:ignore-file
 // GENERATED CODE - DO NOT MODIFY BY HAND
 // ignore_for_file: type=lint
-// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
 
 part of 'models.dart';
 
@@ -917,6 +917,7 @@ class _$_ThemeConfiguration extends _ThemeConfiguration {
   final List<ExtendedTheme> _lightThemes;
   @override
   List<ExtendedTheme> get lightThemes {
+    if (_lightThemes is EqualUnmodifiableListView) return _lightThemes;
     // ignore: implicit_dynamic_type
     return EqualUnmodifiableListView(_lightThemes);
   }
@@ -924,6 +925,7 @@ class _$_ThemeConfiguration extends _ThemeConfiguration {
   final List<ExtendedTheme> _darkThemes;
   @override
   List<ExtendedTheme> get darkThemes {
+    if (_darkThemes is EqualUnmodifiableListView) return _darkThemes;
     // ignore: implicit_dynamic_type
     return EqualUnmodifiableListView(_darkThemes);
   }
@@ -931,6 +933,7 @@ class _$_ThemeConfiguration extends _ThemeConfiguration {
   final List<Font> _fonts;
   @override
   List<Font> get fonts {
+    if (_fonts is EqualUnmodifiableListView) return _fonts;
     // ignore: implicit_dynamic_type
     return EqualUnmodifiableListView(_fonts);
   }
