ac562bd5b0300d55317f50610c194bd60686d6dc
Gaëtan <blaxoujunior@gmail.com>
2023-01-30 18:39:13 +0100

    isolate some packages

--- packages/theming/lib/dynamic_theme.dart
+++
@@ -1,7 +0,0 @@
-// ignore: comment_references
-/// A library that provides a [DynamicTheme] widget that can be used to change the
-/// theme of the application dynamically.
-library dynamic_theme;
-
-export 'src/dynamic_theme.dart';
-export 'src/models.dart';
