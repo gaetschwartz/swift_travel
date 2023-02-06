647c37b8f84a532bdc0c8b2b6d243aaab626da81
Gaëtan <blaxoujunior@gmail.com>
2023-02-06 12:23:22 +0100

    upgrade ci

--- CI/latest.sh
+++ CI/latest.sh
@@ -1,23 +1,40 @@
 #!/usr/bin/env bash
-# fail if any commands fails
-set -e
-# debug log
-set -x
 
 flutter_path=$(which flutter)
-version=$(cat $(dirname $flutter_path)/../version)
-echo "Current flutter version is: $version"
+if [ -z $flutter_path ]; then
+  echo "Flutter not installed"
+  current_version="0.0.0"
+else
+  current_version=$(cat $(dirname $flutter_path)/../version)
+  echo "Current flutter version is: <$current_version>"
+fi
 
-out=$(curl -s "https://storage.googleapis.com/flutter_infra_release/releases/releases_macos.json" | python3 CI/latest.py $1)
+# current arch
+arch=$(uname -m)
+if [ $arch == "x86_64" ]; then
+  arch="x64"
+fi
 
-IFS=',' #setting space as delimiter  
-read -a arr <<< "$out" #reading str as an array as tokens separated by IFS 
+json=$(curl -s "https://storage.googleapis.com/flutter_infra_release/releases/releases_macos.json")
+hash=$(echo $json | jq -r '.current_release.'$1'')
+# echo "Latest hash is: $hash"
+base_url=$(echo $json | jq -r '.base_url')
+version=$(echo $json | jq -r '.releases[] | select(.hash == "'$hash'" and .dart_sdk_arch == "'$arch'")')
+version_string=$(echo $version | jq -r '.version')
+archive_url=$(echo $version | jq -r '.archive')
 
-echo "Latest flutter version is: ${arr[0]}"
+echo "Latest flutter version is: <$version_string>"
 
-if [ $version == ${arr[0]} ]; then
+if [ $current_version == $version_string ]; then
   echo "Up to date"
 else
-  echo "We don't have latest."
-  envman add --key FLUTTER_ARCHIVE_URL --value ${arr[1]}
+  echo "We don't have the target version (target: $version_string, current: $current_version)."
+  # if envman is available, set the env vars for the next step
+  if [ -n "$(which envman)" ]; then
+    echo "Setting env vars for the next step"
+    envman add --key FLUTTER_ARCHIVE_URL --value $archive_url
+  else
+    echo "envman not available, setting regular env vars"
+    export FLUTTER_ARCHIVE_URL="$base_url/$archive_url"
+  fi
 fi
